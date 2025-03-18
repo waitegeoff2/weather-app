02c4a8ce36a58ff0f79e2a94fecb4c118173ffd3
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-18 17:19:29 -0400

    Create new function for DOM, need to move code dow

--- src/index.js
+++ src/index.js
@@ -4,165 +4,166 @@ const searchBar = document.querySelector(".search-input")
 const resultsDiv = document.querySelector(".weather-results");
 
 async function displayWeather(location) {
-
     try {
-    //ADD TRY CATCH
+        resultsDiv.innerHTML = "";
+        
+        // await the fetching of the API
+        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=metric&key=8AP37SHUDAWNDGZNJLSLFKNE9&contentType=json', {mode: 'cors'})
+        // json method returns a promise, we await it to get the json data and turn it into catData
+        const weatherData = await response.json();
+       
 
-    resultsDiv.innerHTML = "";
-    
-    // await the fetching of the API
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=metric&key=8AP37SHUDAWNDGZNJLSLFKNE9&contentType=json', {mode: 'cors'})
-    // json method returns a promise, we await it to get the json data and turn it into catData
-    const weatherData = await response.json();
+        //fetch GIPHY based on current weather conditions
+        const giphyResponse = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=hI6ElrMJLG0BBrhQgzlSKBiKrYPdY2eU&s=" + weatherData.currentConditions.conditions + "skies weather", {mode: 'cors'})
+        const weatherGIPHY = await giphyResponse.json();
 
-    const giphyResponse = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=hI6ElrMJLG0BBrhQgzlSKBiKrYPdY2eU&s=" + weatherData.currentConditions.conditions, {mode: 'cors'})
-    const weatherGIPHY = await giphyResponse.json();
+        appendDOM(weatherData, weatherGIPHY);
 
-    console.log(weatherGIPHY);
+        //APPEND RESULTS TO DOM
 
-    //GIF RESPONSE
+        //location
 
-    //const gif response = URL + weatherdata.currentconditions
-    //append below
+        // const thisLocationTitle = document.createElement("div");
+        // thisLocationTitle.classList.add("weatherLabel");
+        // thisLocationTitle.textContent = "Location";
+        // resultsDiv.appendChild(thisLocationTitle);
 
-    console.log(weatherData);
+        const thisLocation = document.createElement("div");
+        thisLocation.classList.add("weatherResult");
+        thisLocation.textContent = weatherData.resolvedAddress;
+        resultsDiv.appendChild(thisLocation);
 
-    //append results to DOM
+        //conditions
 
-    //location
+        const conditionsTitle = document.createElement("div");
+        conditionsTitle.classList.add("weatherLabel");
+        conditionsTitle.textContent = "Current Conditions";
+        resultsDiv.appendChild(conditionsTitle);
 
-    const thisLocationTitle = document.createElement("div");
-    thisLocationTitle.classList.add("weatherLabel");
-    thisLocationTitle.textContent = "Location";
-    resultsDiv.appendChild(thisLocationTitle);
+        const currentConditions = document.createElement("div");
+        currentConditions.classList.add("weatherResult");
+        currentConditions.textContent = weatherData.currentConditions.conditions;
+        resultsDiv.appendChild(currentConditions);
 
-    const thisLocation = document.createElement("div");
-    thisLocation.classList.add("weatherResult");
-    thisLocation.textContent = weatherData.resolvedAddress;
-    resultsDiv.appendChild(thisLocation);
+        //temperature
+        
+        const tempTitle = document.createElement("div");
+        tempTitle.classList.add("weatherLabel");
+        tempTitle.textContent = "Current Temperature";
+        resultsDiv.appendChild(tempTitle);
 
-    //conditions
+        const currentTemp = document.createElement("div");
+        currentTemp.classList.add("weatherResult");
+        currentTemp.textContent = weatherData.currentConditions.temp;
+        resultsDiv.appendChild(currentTemp);
 
-    const conditionsTitle = document.createElement("div");
-    conditionsTitle.classList.add("weatherLabel");
-    conditionsTitle.textContent = "Current Conditions";
-    resultsDiv.appendChild(conditionsTitle);
+        //feels like
+        const feelsLikeTitle = document.createElement("div");
+        feelsLikeTitle.classList.add("weatherLabel");
+        feelsLikeTitle.textContent = "Feels Like (Temp)";
+        resultsDiv.appendChild(feelsLikeTitle);
 
-    const currentConditions = document.createElement("div");
-    currentConditions.classList.add("weatherResult");
-    currentConditions.textContent = weatherData.currentConditions.conditions;
-    resultsDiv.appendChild(currentConditions);
+        const feelsLike = document.createElement("div");
+        feelsLike.classList.add("weatherResult");
+        feelsLike.textContent = weatherData.currentConditions.feelslike;
+        resultsDiv.appendChild(feelsLike);
 
-    //temperature
-    
-    const tempTitle = document.createElement("div");
-    tempTitle.classList.add("weatherLabel");
-    tempTitle.textContent = "Current Temperature";
-    resultsDiv.appendChild(tempTitle);
+        //precip prob
+        const precipProbTitle = document.createElement("div");
+        precipProbTitle.classList.add("weatherLabel");
+        precipProbTitle.textContent = "Probability of Precipitation (%)";
+        resultsDiv.appendChild(precipProbTitle);
 
-    const currentTemp = document.createElement("div");
-    currentTemp.classList.add("weatherResult");
-    currentTemp.textContent = weatherData.currentConditions.temp;
-    resultsDiv.appendChild(currentTemp);
+        const precipProb = document.createElement("div");
+        precipProb.classList.add("weatherResult");
+        precipProb.textContent = weatherData.currentConditions.precipprob;
+        resultsDiv.appendChild(precipProb);
 
-    //feels like
-    const feelsLikeTitle = document.createElement("div");
-    feelsLikeTitle.classList.add("weatherLabel");
-    feelsLikeTitle.textContent = "Feels Like (Temp)";
-    resultsDiv.appendChild(feelsLikeTitle);
+        //uv index
 
-    const feelsLike = document.createElement("div");
-    feelsLike.classList.add("weatherResult");
-    feelsLike.textContent = weatherData.currentConditions.feelslike;
-    resultsDiv.appendChild(feelsLike);
+        const UVTitle = document.createElement("div");
+        UVTitle.classList.add("weatherLabel");
+        UVTitle.textContent = "UV Index";
+        resultsDiv.appendChild(UVTitle);
 
-    //precip prob
-    const precipProbTitle = document.createElement("div");
-    precipProbTitle.classList.add("weatherLabel");
-    precipProbTitle.textContent = "Probability of Precipitation (%)";
-    resultsDiv.appendChild(precipProbTitle);
+        const UVIndex = document.createElement("div");
+        UVIndex.classList.add("weatherResult");
+        UVIndex.textContent = weatherData.currentConditions.uvindex;
+        resultsDiv.appendChild(UVIndex);
 
-    const precipProb = document.createElement("div");
-    precipProb.classList.add("weatherResult");
-    precipProb.textContent = weatherData.currentConditions.precipprob;
-    resultsDiv.appendChild(precipProb);
+        //sunrise time
 
-    //uv index
+        const SunriseTitle = document.createElement("div");
+        SunriseTitle.classList.add("weatherLabel");
+        SunriseTitle.textContent = "Sunrise Time";
+        resultsDiv.appendChild(SunriseTitle);
 
-    const UVTitle = document.createElement("div");
-    UVTitle.classList.add("weatherLabel");
-    UVTitle.textContent = "UV Index";
-    resultsDiv.appendChild(UVTitle);
+        const sunriseTime = document.createElement("div");
+        sunriseTime.classList.add("weatherResult");
+        sunriseTime.textContent = weatherData.currentConditions.sunrise;
+        resultsDiv.appendChild(sunriseTime);
 
-    const UVIndex = document.createElement("div");
-    UVIndex.classList.add("weatherResult");
-    UVIndex.textContent = weatherData.currentConditions.uvindex;
-    resultsDiv.appendChild(UVIndex);
+        //sunset time
 
-    //sunrise time
+        const sunsetTitle = document.createElement("div");
+        sunsetTitle.classList.add("weatherLabel");
+        sunsetTitle.textContent = "Sunset Time";
+        resultsDiv.appendChild(sunsetTitle);
 
-    const SunriseTitle = document.createElement("div");
-    SunriseTitle.classList.add("weatherLabel");
-    SunriseTitle.textContent = "Sunrise Time";
-    resultsDiv.appendChild(SunriseTitle);
+        const sunsetTime = document.createElement("div");
+        sunsetTime.classList.add("weatherResult");
+        sunsetTime.textContent = weatherData.currentConditions.sunset;
+        resultsDiv.appendChild(sunsetTime);
 
-    const sunriseTime = document.createElement("div");
-    sunriseTime.classList.add("weatherResult");
-    sunriseTime.textContent = weatherData.currentConditions.sunrise;
-    resultsDiv.appendChild(sunriseTime);
+        //forecast heading
 
-    //sunset time
+        const forecastHeading = document.createElement("div");
+        forecastHeading.classList.add("forecastHeading");
+        forecastHeading.textContent = "Tomorrow's Forecast";
+        resultsDiv.appendChild(forecastHeading);
 
-    const sunsetTitle = document.createElement("div");
-    sunsetTitle.classList.add("weatherLabel");
-    sunsetTitle.textContent = "Sunset Time";
-    resultsDiv.appendChild(sunsetTitle);
+        //tomorrow's high
 
-    const sunsetTime = document.createElement("div");
-    sunsetTime.classList.add("weatherResult");
-    sunsetTime.textContent = weatherData.currentConditions.sunset;
-    resultsDiv.appendChild(sunsetTime);
+        const highTmrwTitle = document.createElement("div");
+        highTmrwTitle.classList.add("weatherLabel");
+        highTmrwTitle.textContent = "Tomorrow's Forecasted High";
+        resultsDiv.appendChild(highTmrwTitle);
 
-    //forecast heading
+        const tmrwHigh = document.createElement("div");
+        tmrwHigh.classList.add("weatherResult");
+        tmrwHigh.textContent = weatherData.days[1].tempmax;
+        resultsDiv.appendChild(tmrwHigh);
 
-    const forecastHeading = document.createElement("div");
-    forecastHeading.classList.add("forecastHeading");
-    forecastHeading.textContent = "Tomorrow's Forecast";
-    resultsDiv.appendChild(forecastHeading);
+        //tomorrow's low
 
-    //tomorrow's high
+        const lowTmrwTitle = document.createElement("div");
+        lowTmrwTitle.classList.add("weatherLabel");
+        lowTmrwTitle.textContent = "Tomorrow's Forecasted Low";
+        resultsDiv.appendChild(lowTmrwTitle);
 
-    const highTmrwTitle = document.createElement("div");
-    highTmrwTitle.classList.add("weatherLabel");
-    highTmrwTitle.textContent = "Tomorrow's Forecasted High";
-    resultsDiv.appendChild(highTmrwTitle);
+        const tmrwLow = document.createElement("div");
+        tmrwLow.classList.add("weatherResult");
+        tmrwLow.textContent = weatherData.days[1].tempmin;
+        resultsDiv.appendChild(tmrwLow);
 
-    const tmrwHigh = document.createElement("div");
-    tmrwHigh.classList.add("weatherResult");
-    tmrwHigh.textContent = weatherData.days[1].tempmax;
-    resultsDiv.appendChild(tmrwHigh);
+        //add gif
+        const weatherGIF = document.createElement("img");
+        weatherGIF.classList.add("weatherGIF");
+        weatherGIF.src = weatherGIPHY.data.images.original.url;
+        resultsDiv.appendChild(weatherGIF);
 
-    //tomorrow's low
-
-    const lowTmrwTitle = document.createElement("div");
-    lowTmrwTitle.classList.add("weatherLabel");
-    lowTmrwTitle.textContent = "Tomorrow's Forecasted Low";
-    resultsDiv.appendChild(lowTmrwTitle);
-
-    const tmrwLow = document.createElement("div");
-    tmrwLow.classList.add("weatherResult");
-    tmrwLow.textContent = weatherData.days[1].tempmin;
-    resultsDiv.appendChild(tmrwLow);
-
-    //add gif
-    const weatherGIF = document.createElement("img");
-    weatherGIF.classList.add("weatherGIF");
-    weatherGIF.src = weatherGIPHY.data.images.original.url;
-    resultsDiv.appendChild(weatherGIF);
-
-} catch (error) {
-    alert("Not a valid location, please enter a new one.");
+    } catch (error) {
+        alert("Not a valid location, please enter a new one.");
+    }
 }
+
+function appendDOM(weatherData, giphyData) {
+    // pass that object in and do it here
+    console.log(weatherData);
+    console.log(giphyData);
+
+    //ADD THE DOM OBJECTS HERE
+
 }
 
 searchButton.addEventListener("click", () => {
